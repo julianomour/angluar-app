17246ce38d8c70a3206c997faa3c2ef81d60c28e
Juliano Moura <julianomoura05@gmail.com>
2018-10-14 22:44:53 -0300

    criando componente Cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-teste';
+  title:string = 'Hello World !';
+  subtitle:string = 'Primeiros passos com Angular';
+  
 }
